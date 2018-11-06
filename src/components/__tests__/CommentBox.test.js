9e3796adfbb7294004dc7c95b9142e8e772b63dc
Tiffany Poss <tiffanyposs@gmail.com>
2018-11-05 22:14:10 -0500

    finish CommentBox test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -5,7 +5,7 @@ import CommentBox from 'components/CommentBox';
 // component
 let wrapped;
 
-// run this before each test
+// run this before every test
 beforeEach(() => {
   wrapped = mount(<CommentBox />); // create a full dom render of Comment Box component
 })
@@ -20,18 +20,40 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1); // check for a button
 });
 
-it('has a textarea that users can type in', () => {
+// describe groups a set of tests
+describe('the text area', () => {
 
-  // simulate a change event, add fake data onto the event to simulate a text update
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+  // this beforeEach only runs before the tests within this describe
+  beforeEach(() => {
+    // simulate a change event, add fake data onto the event to simulate a text update
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+
+    // for component to rerender so we don't have to wait for the React queue
+    wrapped.update();
   });
 
-  // for component to rerender so we don't have to wait for the React queue
-  wrapped.update();
 
-  // use the .prop method to pull the property we want to check
-  // using this method of checking the prop instead of the actual value or
-  // text in the textarea box
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  it('has a textarea that users can type in', () => {
+    // use the .prop method to pull the property we want to check
+    // using this method of checking the prop instead of the actual value or
+    // text in the textarea box
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+
+  it('when form is submitted, textarea gets emptied', () => {
+    // check if it actually updates
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+    // find the form and submit it
+    wrapped.find('form').simulate('submit');
+
+    // force udpate
+    wrapped.update();
+
+    // check to see if the textarea is empty
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
